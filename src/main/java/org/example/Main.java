a9b03a55e5f6f523e5a783c8bd9b40cd1d40efea
Afsha Hossain <afsha.hossain@capgemini.com>
2024-05-30 12:05:35 +0100

    Completed 7.1, 7.2 and 7.3

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Car nissan = new Car("Nissan", 10000, 2020, "Green");
+        Car dodge = new Car("Dodge", 11000, 2019, "Blue");
+
+        nissan.setColor("Jet Black");
+        dodge.setColor("Jet Black");
+
+        nissan.setPrice(nissan.getPrice() / 2);
+        dodge.setPrice(dodge.getPrice() / 2);
+
+        /*
+        nissan.make = "Nissan";
+        nissan.price = 10000;
+        nissan.year = 2020;
+        nissan.color = "Green";
+
+        Car dodge = new Car();
+        dodge.make = "Dodge";
+        dodge.price = 11000;
+        dodge.year = 2019;
+        dodge.color = "Blue";
+         */
+
+        System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() +
+                ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
+        System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() +
+                ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
+    }
+}
