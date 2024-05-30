789349aebc6185cdf0a3787460510ab016b1fb82
Afsha Hossain <afsha.hossain@capgemini.com>
2024-05-30 16:42:47 +0100

    Practice exercise 7.4 on Copy Constructor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,27 +5,20 @@ public class Main {
         Car nissan = new Car("Nissan", 10000, 2020, "Green");
         Car dodge = new Car("Dodge", 11000, 2019, "Blue");
 
-        nissan.setColor("Jet Black");
-        dodge.setColor("Jet Black");
+        Car nissan2 = new Car(nissan);
 
-        nissan.setPrice(nissan.getPrice() / 2);
-        dodge.setPrice(dodge.getPrice() / 2);
+        nissan2.setColor("Yellow");
+        nissan.setColor("Orange");
+        nissan2.setColor("Blue");
+        nissan.setColor("Purple");
+        nissan2.setColor("Fuchsia");
+        nissan.setColor("Beige");
 
-        /*
-        nissan.make = "Nissan";
-        nissan.price = 10000;
-        nissan.year = 2020;
-        nissan.color = "Green";
-
-        Car dodge = new Car();
-        dodge.make = "Dodge";
-        dodge.price = 11000;
-        dodge.year = 2019;
-        dodge.color = "Blue";
-         */
 
         System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() +
                 ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
+        System.out.println("This " + nissan2.getMake() + " is worth $" + nissan2.getPrice() +
+                ". It was built in " + nissan2.getYear() + ". It is " + nissan2.getColor() + ".\n");
         System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() +
                 ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
     }
